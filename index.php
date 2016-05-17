0ce41b56c033bcff940dcd8538322fadbcb5cee6
Jos Piek <jos.piek@kpnmail.nl>
2016-05-17 11:24:43 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>EenmaalAndermaal</title>
+
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/template.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="./images/logo.png" alt="EenmaalAndermaal">
+
+                    <h1>EenmaalAndermaal</h1>
+                </div>
+            </div>
+
+            <nav class="navbar navbar-default navbar-template">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Home</a></li>
+                            <li><a href="#">Veilingen</a></li>
+                            <li><a href="#">Start nieuwe vieling</a></li>
+                            <li><a href="#">Aanmaken verkoopaccount</a></li>
+                        </ul>
+
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#">Login</a></li>
+                            <li class="active"><a href="#">Registreren</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <div class="row">
+                <div class="col-md-12">
+
+                </div>
+            </div>
+
+
+            <div class="row">
+            </div>
+        </div>
+
+
+        <script src="./js/jquery-1.12.3.min.js"></script>
+        <script src="./js/bootstrap.min.js"></script>
+    </body>
+</html>
