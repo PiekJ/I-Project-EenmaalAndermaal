0bffb1ca409648bf18887317e34631859aee35c4
tristandr <tristanderoo@gmail.com>
2016-05-18 14:23:14 +0200

    verbeterd

--- php/mail.php
+++ php/mail.php
@@ -1,14 +1,15 @@
 <?php
-    if($_SERVER[REQUEST_METHOD] == 'POST'){
-        sendMail($_POST[email]);
-    }
     $char = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxz";
     $message = "Bedankt voor het bezoeken van de veilingwebsite EenmaalAndermaal!<br>
                 Vul de volgende code <a href=#>hier</a> in: createCode()";
+
+    if($_SERVER[REQUEST_METHOD] == 'POST'){
+        sendMail($_POST[email]);
+    }
     function sendMail($email){
         mail($email, "EenmaalAndermaal registratiecode", )
     }
     function createCode(){
-        return substr($char, 0, 6);
+        return substr(str_shuffle($char), 0, 6);
     }
 ?>
