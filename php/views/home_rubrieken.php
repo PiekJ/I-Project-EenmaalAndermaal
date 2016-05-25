1196e67e1b78eacd21c0a91d45534c22272ed16c
Jos Piek <jos.piek@kpnmail.nl>
2016-05-25 13:36:11 +0200

    veiling geneuzel

--- php/views/home_rubrieken.php
+++ php/views/home_rubrieken.php
@@ -1,4 +1,5 @@
 <select name="rubriek" class="form-control rubriek-form-control">
+    <option value="0">Alle rubrieken</option>
 <?php foreach (get_data_view('rubrieken') as $rubriek) {
     printf('<option value="%d">%s</option>', $rubriek['rubrieknummer'], str_replace('  ', '&nbsp;&nbsp;', $rubriek['rubrieknaam']));
 } ?>
