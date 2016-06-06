4dbb4e55dc99e63b210e1dfd397b6fb3421b54c1
Jos Piek <jos.piek@kpnmail.nl>
2016-06-06 14:55:17 +0200

    User email fix

--- php/user.php
+++ php/user.php
@@ -225,7 +225,7 @@
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-        @mail($email, "EenmaalAndermaal registratie afgerond", $message, $headers)
+        @mail($email, "EenmaalAndermaal registratie afgerond", $message, $headers);
         
         return true;
     }
